04e532bba4be51b9dc870524ac7e9bc3bc585030
Afonso F <afonso.franca@gmail.com>
2016-10-02 15:04:10 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# PostCSS Encode Base64 Inlined Images [![Build Status][ci-img]][ci]
+
+[PostCSS] plugin encode with base64 inlined images.
+
+[PostCSS]: https://github.com/postcss/postcss
+[ci-img]:  https://travis-ci.org/afonsof/postcss-encode-base64-inlined-images.svg
+[ci]:      https://travis-ci.org/afonsof/postcss-encode-base64-inlined-images
+
+```css
+.foo {
+    /* Input example */
+}
+```
+
+```css
+.foo {
+  /* Output example */
+}
+```
+
+## Usage
+
+```js
+postcss([ require('postcss-encode-base64-inlined-images') ])
+```
+
+See [PostCSS] docs for examples for your environment.
---
+++ index.js
@@ -0,0 +1,18 @@
+var postcss = require('postcss');
+var btoa = require('btoa');
+
+module.exports = postcss.plugin('postcss-encode-base64-inlined-images', function (opts) {
+    opts = opts || {};
+
+    return function (css, result) {
+        css.walkDecls('background-image', function (decl) {
+            decl.value = decl.value.replace(/url\(["']data/g, 'url(data');
+            decl.value = decl.value.replace(/["']\)$/g, ')');
+
+            if (decl.value.indexOf('url(data:image/svg+xml;charset=UTF-8,') === 0) {
+                var encodedFile = decl.value.substring(decl.value.indexOf(',') + 1, decl.value.length - 1);
+                decl.value = 'url(data:image/svg+xml;base64,' + btoa(decodeURIComponent(encodedFile)) + ')';
+            }
+        });
+    };
+});
